84d4334579282a2dc65da1e95993b71d0eb6df8a
Antony <antony.budianto@tokopedia.com>
2020-02-03 08:19:43 +0700

    add static serve

--- server.js
+++ server.js
@@ -5,9 +5,15 @@ var upload = multer({ dest: "uploads/" });
 
 var app = express();
 
+app.use("/files", express.static("uploads"));
 app.post("/api/file", upload.single("file"), function(req, res, next) {
   console.log(req.file);
-  res.json(req.file);
+  const file = {
+    size: req.file.size,
+    mimetype: req.file.mimetype,
+    link: `http://localhost:5000/files/${req.file.filename}`
+  };
+  res.json(file);
 });
 
 module.exports = app;
